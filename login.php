33c7ad1b7acbe91e69534aa23a4b853ec0b51d23
ElenaGir <elena.girard@gmail.com>
2022-05-28 17:27:57 +0200

    fin integration visuelle

--- login.php
+++ login.php
@@ -11,32 +11,27 @@
         <link rel="stylesheet" href="style.css" media="screen" type="text/css" />
         <title>Document</title>
     </head>
-    <body class="bg-[#191720] w-full h-screen flex justify-center  lg:justify-between">
-        <img src="assets/image.png" class="h-full hidden lg:flex">
-        <div id="container" class="text-white flex-col w-10/12 lg:w-2/5 mt-16 lg:mt-2">
-            <div class="hidden">
-                <h1 class="text-xl">Binary</h1>
-                <p class="pt-10">We are</p>
-                <h2 class="text-2xl pt-8">Invite online right now.</h2>
-                <p class="pt-2">10 people have joined our team.</p>
-                <p>We invite you to join the tribe.</p>
-            </div>
-            <!-- zone de connexion -->
-            <form action="verification.php" method="POST" class="flex flex-col lg:w-10/12 lg:mt-20">
+    <body class="bg-[#191720] w-full h-screen flex flex-col items-center lg:items-end">
+            <img src="assets/image.png" class="h-full hidden lg:flex lg:-z-10 absolute left-0">
+            <header class="text-white w-10/12 mt-16 lg:w-5/12 lg:mt-8">
                 <p class="hidden lg:flex text-5xl font-bold">Sign up</p>
-                <h1 class="text-xl lg:hidden">Binary</h1>
-                <p class="pt-10 lg:hidden">We are</p>
-                <h2 class="text-2xl pt-8 lg:hidden">Invite online right now.</h2>
-                <p class="pt-2 lg:hidden">10 people have joined our team.</p>
-                <p class="lg:hidden">We invite you to join the tribe.</p>
-                
+                <div class="lg:flex lg:flex-col  lg:rounded-3xl lg:backdrop-blur-3xl lg:w-4/12 lg:h-5/6 lg:absolute lg:left-28">
+                    <h1 class="text-xl lg:p-4 lg:text-2xl lg:bg-[#7377D5] lg:w-fit lg:mt-8 lg:ml-8">Binary</h1>
+                    <p class="pt-10 lg:p-8">We are</p>
+                    <h2 class="text-2xl pt-8 lg:pt-2 lg:p-8">Invite online right now.</h2>
+                    <p class="pt-2 lg:p-0 lg:pl-8">10 people have joined our team.</p>
+                    <p class="lg:p-0 lg:pl-8">We invite you to join the tribe.</p>                   
+                </div>     
+            </header>
+            <!-- zone de connexion -->
+            <form action="verification.php" method="POST" class="flex flex-col lg:w-5/12 lg:mt-8 w-10/12 text-white lg:pr-24">
                 <label class="pb-4 pt-2 lg:pt-10 lg:text-xl"><b>Email address</b></label>
                 <input type="email" name="email" id="email" class="lg:pl-5 lg:mt-5 h-12 bg-[#1E1C24] border-[#34333C] border-2 invalid:border-red-500 rounded-lg lg:h-16">
                 <p class="invisible m-3" id="errorEmail">Please enter a valid address mail</p>
 
                 <label class="pb-2 pt-2 lg:pt-2 lg:text-xl"><b>Password</b></label>
                 <input id="password" type="password" placeholder="" name="password" class="lg:pl-5 lg:mt-5 h-12 bg-[#1E1C24] border-[#34333C] border-2 invalid:border-red-500 rounded-lg lg:h-16">
-                <p class="invisible m-3 " id="errorPassword">Please enter a valid password</p>
+                <p class="invisible m-3" id="errorPassword">Please enter a valid password</p>
 
                 <input type="submit" id='submit' value='Sign up'class=" mt-2 lg:mt-2 lg:h-16 h-12 bg-gradient-to-r from-[#6871FC] to-[#393DDC] rounded-xl font-lg font-bold">
                 <hr WIDTH="100%"  ALIGN="center" class="mt-14 border-[#34333C] hidden lg:flex">
@@ -49,11 +44,10 @@
                
                 ?>
             </form>
-            <div class="absolute bottom-8 right-8 flex lg:right-28">
-                 <p class="text-[#616066]"> Already have an account?</p>  
-                 <p>Sign in</p>       
-            </div>    
-        </div> 
+            <div class="flex flex-row-reverse mt-14 lg:mr-18 w-10/12 lg:w-1/2 lg:pr-24">
+                <p class="text-sm  text-[#616066] lg:text-xl">Already have an account ? <span class="font-bold text-white">Sign in</span></p>
+            </div>  
+      
         <script src="script.js"></script>     
     </body>
     </html>
